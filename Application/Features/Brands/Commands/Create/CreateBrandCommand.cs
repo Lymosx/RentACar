3327869cfc83bc09b098fc8f158b29eee2e8ce0a
Mehmet Akif <akifdogen@msn.com>
2023-08-03 03:14:46 +0300

    Set 1-2 : CQRS ve MediatR implementasyonu.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse response = new CreatedBrandResponse();
+            response.Id = new Guid();
+            response.Name = request.Name;
+            return Task.FromResult(response);
+        }
+    }
+}
